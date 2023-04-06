d10140c1f27e6154aa3487015ec1be1986084412
nliebers <nliebers24@gmail.com>
2023-04-05 22:45:37 -0500

    Added simple method for attaching all corresponding answers to a question.

---
+++ get_qa_threads.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+class Question:
+    def __init__(self, question_id, title, answers, tags, body, accepted_answer_id):
+        self.question_id = question_id
+        self.title = title
+        self.answers = answers
+        self.tags = tags
+        self.body = body
+        self.accepted_answer = accepted_answer_id
+
+
+class Answer:
+    def __init__(self, answer_id, parent_question_id, body):
+        self.answer_id = answer_id
+        self.parent_question_id = parent_question_id
+        self.body = body
+
+
+all_python_questions = []
+python_db = pd.read_pickle('data/python_database.pkl')
+# Remove all questions that have 0 answers
+python_db = python_db[python_db.question_answer_count != 0]
+current_qa_pointer = python_db.iloc[0]
+question = Question(question_id=current_qa_pointer['question_id'], title=current_qa_pointer['question_title'],
+                    answers=[], tags=current_qa_pointer['question_tags'], body=current_qa_pointer['question_body'],
+                    accepted_answer_id=['question_accepted_answer_id'])
+for index, row in python_db.iterrows():
+    if current_qa_pointer['question_id'] != row['question_id']:
+        all_python_questions.append(question)
+        question = Question(question_id=row['question_id'],
+                            title=row['question_title'],
+                            answers=[], tags=row['question_tags'],
+                            body=row['question_body'],
+                            accepted_answer_id=['question_accepted_answer_id'])
+    answer = Answer(answer_id=row['answer_id'], parent_question_id=row['answer_parent_id'], body=row['answer_body'])
+    question.answers.append(answer)
+    current_qa_pointer = row
+all_python_questions.append(question)
+
+for answer in all_python_questions[len(all_python_questions)-1].answers:
+    print(answer.body)
