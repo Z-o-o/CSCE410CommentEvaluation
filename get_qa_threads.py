a7cccc6f10de979a25d089960dd2bc016cffc344
Brady Johnson <bradyjohnson84@gmail.com>
2023-04-08 02:35:26 -0500

    Rank answers basde on score

--- get_qa_threads.py
+++ get_qa_threads.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 class Question:
@@ -12,36 +13,50 @@ class Question:
 
 
 class Answer:
-    def __init__(self, answer_id, parent_question_id, body):
+    def __init__(self, answer_id, parent_question_id, body, score):
         self.answer_id = answer_id
         self.parent_question_id = parent_question_id
+        self.score = score
         self.body = body
 
 
-all_python_questions = []
-python_db = pd.read_pickle('data/python_database.pkl')
-# Remove all questions that have 0 answers
-python_db = python_db[python_db.question_answer_count != 0]
-current_qa_pointer = python_db.iloc[0]
-question = Question(question_id=current_qa_pointer['question_id'], title=current_qa_pointer['question_title'],
-                    answers=[], tags=current_qa_pointer['question_tags'], body=current_qa_pointer['question_body'],
-                    accepted_answer=None)
-for index, row in python_db.iterrows():
-    if current_qa_pointer['question_id'] != row['question_id']:
-        all_python_questions.append(question)
-        question = Question(question_id=row['question_id'],
-                            title=row['question_title'],
-                            answers=[], tags=row['question_tags'],
-                            body=row['question_body'],
-                            accepted_answer=None)
-    answer = Answer(answer_id=row['answer_id'], parent_question_id=row['answer_parent_id'], body=row['answer_body'])
-    try:
-        if answer.answer_id == row['question_accepted_answer_id']:
-            question.accepted_answer = answer
-    except:
-        print(f"unable to compare {answer.answer_id} and {row['question_accepted_answer_id']}")
-    question.answers.append(answer)
-    current_qa_pointer = row
-all_python_questions.append(question)
+def itemize_data(filename):
+    all_questions = []
+    python_db = pd.read_pickle(filename)
+    # Remove all questions that have 0 answers
+    python_db = python_db[python_db.question_answer_count != 0]
+    current_qa_pointer = python_db.iloc[0]
+    question = Question(question_id=current_qa_pointer['question_id'], title=current_qa_pointer['question_title'],
+                        answers=[], tags=current_qa_pointer['question_tags'], body=current_qa_pointer['question_body'],
+                        accepted_answer=None)
+    for index, row in python_db.iterrows():
+        if current_qa_pointer['question_id'] != row['question_id']:
+            all_questions.append(question)
+            question = Question(question_id=row['question_id'],
+                                title=row['question_title'],
+                                answers=[], tags=row['question_tags'],
+                                body=row['question_body'],
+                                accepted_answer=None)
+        answer = Answer(answer_id=row['answer_id'], parent_question_id=row['answer_parent_id'], score=row['score'], body=row['answer_body'])
+        try:
+            if answer.answer_id == row['question_accepted_answer_id']:
+                question.accepted_answer = answer
+        except:
+            #print(f"unable to compare {answer.answer_id} and {row['question_accepted_answer_id']}")
+            continue
+        question.answers.append(answer)
+        current_qa_pointer = row
+    all_questions.append(question)
 
-print(all_python_questions[len(all_python_questions)-1].accepted_answer.answer_id)
+    return all_questions
+
+
+def rank_answers(all_questions):
+    for question in all_questions:
+        question.answers = sorted(question.answers, key=lambda x: x.score, reverse=True)
+
+    return all_questions
+
+python_data = itemize_data('data/python_database.pkl')
+ranked = rank_answers(python_data)
+
