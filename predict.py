6d0e98fe1d6600ad1df1c3c3ab4b2a518fd580b4
Brady Johnson <bradyjohnson84@gmail.com>
2023-04-26 17:09:46 -0500

    Add confidence and main function

--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 from transformers import BertTokenizer
 import torch
+import argparse
 import custom_dataset
+import model
+import get_qa_threads
 
 
 def predict(model, text):
@@ -16,8 +19,26 @@ def predict(model, text):
     input_id = text_dict["input_ids"].squeeze(1).to(device)
     with torch.no_grad():
         output = model(input_id, mask)
+        probs = torch.nn.functional.softmax(output, dim=1)
+        conf, classes = torch.max(probs, 1)
         label_id = output.argmax(dim=1).item()
         for key in custom_dataset.labels.keys():
             if custom_dataset.labels[key] == label_id:
                 print(text, " => ", key, "#", label_id)
+                print(conf)
+                print(conf.item())
                 break
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--text", "-t")
+
+
+if __name__ == "__main__":
+    custom_model = model.BertClassifier()
+    custom_model.load_state_dict(torch.load("test-model.pth"))
+    predict(
+        custom_model,
+        """Does Chunders like Sake?""",
+    )
